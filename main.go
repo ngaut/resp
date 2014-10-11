ff222cdd80bd7c6d1e05539ffe90000288c70678
liuqi <liuqi@wandoujia.com>
2014-10-11 14:29:08 +0800

    add resp writer

--- main.go
+++ main.go
@@ -23,8 +23,10 @@
 package resp
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"io"
 	"reflect"
 	"strconv"
 )
@@ -200,4 +202,38 @@ func redisMessageToType(dst reflect.Value, out *Message) error {
 	}
 
 	return fmt.Errorf(ErrUnsupportedConversion.Error(), byteToTypeName(out.Type), dstKind)
+}
+
+var (
+	arrayPrefixSlice      = []byte{'*'}
+	bulkStringPrefixSlice = []byte{'$'}
+	lineEndingSlice       = []byte{'\r', '\n'}
+)
+
+type RESPWriter struct {
+	*bufio.Writer
+}
+
+func NewRESPWriter(writer io.Writer) *RESPWriter {
+	return &RESPWriter{
+		Writer: bufio.NewWriter(writer),
+	}
+}
+
+func (w *RESPWriter) WriteCommand(args ...string) (err error) {
+	// Write the array prefix and the number of arguments in the array.
+	w.Write(arrayPrefixSlice)
+	w.WriteString(strconv.Itoa(len(args)))
+	w.Write(lineEndingSlice)
+
+	// Write a bulk string for each argument.
+	for _, arg := range args {
+		w.Write(bulkStringPrefixSlice)
+		w.WriteString(strconv.Itoa(len(arg)))
+		w.Write(lineEndingSlice)
+		w.WriteString(arg)
+		w.Write(lineEndingSlice)
+	}
+
+	return w.Flush()
 }
